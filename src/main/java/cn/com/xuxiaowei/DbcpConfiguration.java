ab1c8ce513eb156495c20cecdb482a000714546a
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2019-12-25 21:05:42 +0800

    Initial commit

---
+++ src/main/java/cn/com/xuxiaowei/DbcpConfiguration.java
@@ -0,0 +1,84 @@
+package cn.com.xuxiaowei;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * DBCP 示例
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+public class DbcpConfiguration {
+
+    public static void main(String[] args) {
+
+        DbcpConfiguration dbcpConfiguration = new DbcpConfiguration();
+
+        dbcpConfiguration.mysql();
+
+        dbcpConfiguration.oracle();
+
+    }
+
+    public void mysql() {
+
+        // 创建连接池示例
+        BasicDataSource dataSource = new BasicDataSource();
+
+        // 设置连接池所需驱动
+        dataSource.setDriverClassName(com.mysql.cj.jdbc.Driver.class.getName());
+
+        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/dbcp?useSSL=false&serverTimezone=GMT%2B8");
+
+        dataSource.setUsername("root");
+
+        dataSource.setPassword("root");
+
+        // 设置连接池初始连接数
+        dataSource.setInitialSize(5);
+
+        // 设置连接池最少的空闲连接数
+        dataSource.setMinIdle(2);
+
+        try (Connection connection = dataSource.getConnection()) {
+
+            System.out.println(connection);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void oracle() {
+
+        // 创建连接池示例
+        BasicDataSource dataSource = new BasicDataSource();
+
+        // 设置连接池所需驱动
+        dataSource.setDriverClassName(oracle.jdbc.driver.OracleDriver.class.getName());
+
+        dataSource.setUrl("jdbc:oracle:thin:@192.168.8.128:1521:ORCL");
+
+        dataSource.setUsername("scott");
+
+        dataSource.setPassword("orcl");
+
+        // 设置连接池初始连接数
+        dataSource.setInitialSize(5);
+
+        // 设置连接池最少的空闲连接数
+        dataSource.setMinIdle(2);
+
+        try (Connection connection = dataSource.getConnection()) {
+
+            System.out.println(connection);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
